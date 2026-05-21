13e4215468d45a6b45d5e5f424ddd3b209c1848c
AJ Anderson <79423420+ajanderson1@users.noreply.github.com>
2026-05-21 12:39:32 +0200

    chore(infra): pin opentofu + provider versions

---
+++ infra/versions.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_version = ">= 1.8.0"
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0"
+    }
+    hcloud = {
+      source  = "hetznercloud/hcloud"
+      version = "~> 1.45"
+    }
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 4.0"
+    }
+    cloudinit = {
+      source  = "hashicorp/cloudinit"
+      version = "~> 2.3"
+    }
+  }
+}
